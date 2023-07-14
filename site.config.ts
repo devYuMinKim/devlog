d37e50bd4695be4ea3348d8b441935e32fbfcb26
devYuMinKim <gimyumin40@gmail.com>
2023-07-14 17:40:10 +0900

    🎨 refactor : Modify rootNotionPageId

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config';
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'd91df49e51034f9e864dbe36cdeda918',
+  rootNotionPageId: '790d9682fa1c4acea4b8c76766857443',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
